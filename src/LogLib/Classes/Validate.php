7f2332e22876f5eeb4b900d5ab5225d0c015ebcc
Netkas <netkas@nosial.net>
2023-10-12 17:16:02 -0400

    Fix

--- src/LogLib/Classes/Validate.php
+++ src/LogLib/Classes/Validate.php
@@ -9,10 +9,10 @@
         /**
          * Checks if the given level is a valid level type.
          *
-         * @param string $level The level to check.
+         * @param int $level The level to check.
          * @return bool Returns true if the level is valid
          */
-        public static function LevelType(string $level): bool
+        public static function LevelType(int $level): bool
         {
             return in_array($level, LevelType::ALL);
         }
@@ -20,19 +20,17 @@
         /**
          * Checks if the given input level is valid for the current level.
          *
-         * @param string $input The input level to check.
-         * @param string $current_level The current level to compare against.
+         * @param int $input The input level to check.
+         * @param int $current_level The current level to compare against.
          * @return bool Returns true if the input level is valid for the current level, false otherwise.
          */
-        public static function checkLevelType(string $input, string $current_level): bool
+        public static function checkLevelType(int $input, int $current_level): bool
         {
-            $input = strtolower($input);
             if(!self::LevelType($input))
             {
                 return false;
             }
 
-            $current_level = strtolower($current_level);
             if(!self::LevelType($current_level))
             {
                 return false;
