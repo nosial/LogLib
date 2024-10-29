92107d4018255643f415c9f238df2bbe4a7ccb65
netkas <netkas@n64.cc>
2024-10-29 00:38:37 -0400

    Updated LogTest

---
+++ tests/LogLib/LogTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace LogLib;
+
+use PHPUnit\Framework\TestCase;
+
+class LogTest extends TestCase
+{
+
+    public function testDebug()
+    {
+
+    }
+
+    public function testInfo()
+    {
+
+    }
+
+    public function testGetRuntimeOptions()
+    {
+
+    }
+
+    public function testGetOptions()
+    {
+
+    }
+
+    public function testGetApplication()
+    {
+
+    }
+
+    public function testUnregisterExceptionHandler()
+    {
+
+    }
+
+    public function testUnregister()
+    {
+
+    }
+
+    public function testWarning()
+    {
+
+    }
+
+    public function testError()
+    {
+
+    }
+
+    public function testFatal()
+    {
+
+    }
+
+    public function testRegisterExceptionHandler()
+    {
+
+    }
+
+    public function testVerbose()
+    {
+
+    }
+
+    public function testRegister()
+    {
+
+    }
+}
